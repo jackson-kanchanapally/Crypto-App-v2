521e0343637ec2e9fede08345ab4cbb838410b64
Jackson kanchanapally <92465955+kanchanapallyjackson@users.noreply.github.com>
2022-10-07 02:09:36 +0530

    Header Created

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import {React,useState} from "react";
 import {
   HStack,
   Heading,
@@ -8,33 +8,34 @@ import {
   useColorMode,
   useColorModeValue
 } from "@chakra-ui/react";
-import { FaSun, FaGithub } from "react-icons/fa";
+import { FaMoon,FaSun, FaGithub } from "react-icons/fa";
 
 export default function Header() {
-    const {toggleColorMode}=useColorMode();
+    const {colorMode,toggleColorMode}=useColorMode();
+    const isDark=colorMode==='dark'
   const [isNotSmallerScreen] =useMediaQuery("(min-width:600px)")
   return (
     <HStack
-      bg="gray.900"
+      bg={isDark?"gray.900":"gray.300"}
       height="16"
       alignContent={"center"}
       alignItems="center"
       justifyContent="space-between"
       width="100vw"
     >
-      <Heading color="goldenrod" pl={isNotSmallerScreen?"10":"5"} fontSize={isNotSmallerScreen?"30":'25'}>
-        Crypto
+      <Heading  pl={isNotSmallerScreen?"10":"5"} fontSize={isNotSmallerScreen?"30":'25'}>
+        Crypto 
       </Heading>
       <HStack justifyContent={"space-between"} width={isNotSmallerScreen?"30vw":"46vw"}>
-        <Select width={isNotSmallerScreen?"30":"40"} placeholder="select" fontSize="xs">
+        <Select variant='outline' borderColor={isDark?"gray.700":"gray.500"} width={isNotSmallerScreen?"30":"40"} fontSize="xs">
           <option value="usd">INR</option>
           <option value="inr">USD</option>
         </Select>
         <HStack pr="20">
-          <IconButton mr={isNotSmallerScreen?"5":"2"} icon={<FaSun />} color="white" bg="gray" isRound />
+          <IconButton mr={isNotSmallerScreen?"5":"2"} icon={isDark?<FaMoon/>:<FaSun />} onClick={toggleColorMode} color="gray.200" isRound bg={isDark?"gray.600":"gray.600"}  _hover={isDark?{background: "gray.800"}:{background:"gray.400"}} />
           <a href="https://github.com/jackson-kanchanapally" >
             
-            <IconButton icon={<FaGithub />} color="black"onClick={toggleColorMode} isRound fontSize='25'/>
+            <IconButton icon={<FaGithub />} isRound color='gray.200' fontSize='25' bg={isDark?"gray.600":"gray.600"}  _hover={isDark?{background: "gray.800"}:{background:"gray.400"}} />
           </a>
         </HStack>
       </HStack>
--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ChakraProvider>
-    <ColorModeScript initialColorMode='light'></ColorModeScript>
+    <ColorModeScript initialColorMode='light'>
+    </ColorModeScript>
     <App/>
     </ChakraProvider>
   </React.StrictMode>
